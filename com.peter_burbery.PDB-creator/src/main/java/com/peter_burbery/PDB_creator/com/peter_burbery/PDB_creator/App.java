70267cb71d5774efc6d1b6f0473f5fba2dc6b73c
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-11-07 06:34:24 -0500

    add
    generated files

---
+++ com.peter_burbery.PDB-creator/src/main/java/com/peter_burbery/PDB_creator/com/peter_burbery/PDB_creator/App.java
@@ -0,0 +1,10 @@
+package com.peter_burbery.PDB_creator.com.peter_burbery.PDB_creator;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ com.peter_burbery.PDB-creator/src/test/java/com/peter_burbery/PDB_creator/com/peter_burbery/PDB_creator/AppTest.java
@@ -0,0 +1,19 @@
+package com.peter_burbery.PDB_creator.com.peter_burbery.PDB_creator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
